0e087729eb54bfad1c97b6aa19b41e32f36002a5
prashantkolhe240 <prashantkolhe240@gmail.com>
2023-02-11 22:47:32 +0530

    day1 program uploaded

---
+++ operator1.js
@@ -0,0 +1,27 @@
+console.log("p" + "\n" + "K");
+
+//relational and comparission operator operator 
+console.log(2>=5);// false
+console.log(5>=1);//true
+
+console.log(4<=10);//true
+console.log(5<=4);//false
+
+
+//comparission operator
+//==== check data type and both values 
+console.log(3===3);//true
+
+console.log(3=="3");
+console.log(3==4);
+console.log(7==="7");
+
+let a=10;
+let b=20;
+
+console.log(((a+b) > (b-a)) <= ((b/a) > (a%b))); 
+console.log("one"<7);
+
+let x = 3;
+let y = 1;
+console.log(((x+y)**(x-y))*((y**2)+(x**3)));
---
+++ problem2.js
@@ -0,0 +1,11 @@
+// My name 
+let name= "Prashant Kolhe";
+console.log(name);
+
+// my father name 
+name="Dhanraj Kolhe";
+console.log(name);
+
+// my mother name 
+name="Sangita Kolhe";
+console.log(name);
